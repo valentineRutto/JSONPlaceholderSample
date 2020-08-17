fe76a3c9eed9ea78638f4bd6d33b17bf23c8cbd8
valentinerutto <rutto28@gmail.com>
2020-08-17 12:28:26 +0300

    -add recyclerviewadapter code

--- app/src/main/java/com/example/jsonplaceholdersample/adapters/PostsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/jsonplaceholdersample/adapters/PostsRecyclerViewAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.jsonplaceholdersample.adapters
 
-class PostsRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jsonplaceholdersample.R
+import com.example.jsonplaceholdersample.network.data.response.PostResponse
+import kotlinx.android.synthetic.main.row_post_item.view.*
+
+class PostsRecyclerViewAdapter(var listener: clickListener) :
+    RecyclerView.Adapter<PostsRecyclerViewAdapter.ViewHolder>() {
+
+    var postList: List<PostResponse> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.row_post_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        return holder.bindData(postList[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindData(postResponse: PostResponse) {
+            itemView.txt_title.text = postResponse.title
+            itemView.txt_body.text = postResponse.body
+
+        }
+    }
+
+    fun addData(postlist: PostResponse) {
+
+    }
+
+    interface clickListener {
+        fun showPost()
+    }
 }
